39c11696e287bed43ad3aedea5d0fbae45205756
liufan <flanliulf@gmail.com>
2021-01-27 14:19:19 +0800

    【fixe】修复 atAll 为 false，atMobiles 为 null 导致的空指针异常

--- src/main/java/com/fancyliu/dingtalk/robot/model/TextEntity.java
+++ src/main/java/com/fancyliu/dingtalk/robot/model/TextEntity.java
@@ -18,70 +18,76 @@ import java.util.List;
 @Data
 public class TextEntity extends BaseMsgEntity {
 
-    /**
-     * 消息类型,这里为文本类型
-     */
-    private String msgType;
+	/**
+	 * 消息类型,这里为文本类型
+	 */
+	private String msgType;
 
-    /**
-     * 钉钉消息内容
-     */
-    private String content;
+	/**
+	 * 钉钉消息内容
+	 */
+	private String content;
 
-    /**
-     * 是否@所有人
-     */
-    private Boolean isAtAll;
+	/**
+	 * 是否@所有人
+	 */
+	private Boolean isAtAll;
 
-    /**
-     * 被@人的手机号(在content里添加@人的手机号)
-     */
-    private List<String> atMobiles;
+	/**
+	 * 被@人的手机号(在content里添加@人的手机号)
+	 */
+	private List<String> atMobiles;
 
-    public String getMsgType() {
-        return "text";
-    }
+	public String getMsgType() {
+		return "text";
+	}
 
-    @Override
-    public String convertToJsonString() {
-        // text类型
-        JSONObject content = new JSONObject();
-        content.put("content", this.getContent());
+	@Override
+	public String convertToJsonString() {
+		// text类型
+		JSONObject content = new JSONObject();
+		content.put("content", this.getContent());
 
-        // at some body
-        JSONObject atMobile = new JSONObject();
-        if (this.getAtMobiles().size() > 0) {
-            List<String> mobiles = new ArrayList<String>();
-            for (int i = 0; i < this.getAtMobiles().size(); i++) {
-                mobiles.add(this.getAtMobiles().get(i));
-            }
-            if (mobiles.size() > 0) {
-                atMobile.put("atMobiles", mobiles);
-            }
-            atMobile.put("isAtAll", this.getIsAtAll());
-        }
+		// at some body
+		JSONObject atMobile = new JSONObject();
+		if (this.atMobiles != null && this.getAtMobiles().size() > 0) {
+			List<String> mobiles = new ArrayList<String>();
 
-        JSONObject object = new JSONObject();
-        object.put("msgtype", this.getMsgType());
-        object.put("text", content);
-        object.put("at", atMobile);
-        return object.toJSONString();
-    }
+			for (int i = 0; i < this.getAtMobiles().size(); i++) {
+				mobiles.add(this.getAtMobiles().get(i));
+			}
 
-    public TextEntity() {
-    }
+			if (mobiles.size() > 0) {
+				atMobile.put("atMobiles", mobiles);
+			} else {
+				atMobile.put("atMobiles", "");
+			}
+		} else {
+			atMobile.put("atMobiles", "");
+		}
+		atMobile.put("isAtAll", this.getIsAtAll());
 
-    public TextEntity(String msgType, String content, Boolean isAtAll, List<String> atMobiles) {
-        this.msgType = msgType;
-        this.content = content;
-        this.isAtAll = isAtAll;
-        this.atMobiles = atMobiles;
-    }
+		JSONObject object = new JSONObject();
+		object.put("msgtype", this.getMsgType());
+		object.put("text", content);
+		object.put("at", atMobile);
+		return object.toJSONString();
+	}
 
-    public TextEntity(String content, Boolean isAtAll, List<String> atMobiles) {
-        this.content = content;
-        this.isAtAll = isAtAll;
-        this.atMobiles = atMobiles;
-        this.msgType = "text";
-    }
+	public TextEntity() {
+	}
+
+	public TextEntity(String msgType, String content, Boolean isAtAll, List<String> atMobiles) {
+		this.msgType = msgType;
+		this.content = content;
+		this.isAtAll = isAtAll;
+		this.atMobiles = atMobiles;
+	}
+
+	public TextEntity(String content, Boolean isAtAll, List<String> atMobiles) {
+		this.content = content;
+		this.isAtAll = isAtAll;
+		this.atMobiles = atMobiles;
+		this.msgType = "text";
+	}
 }
